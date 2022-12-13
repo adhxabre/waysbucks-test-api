1268d7d1c1442777d6aa87d4921c659042c65f5b
adhxabre <abeldustin06@gmail.com>
2022-12-13 09:25:47 +0700

    Update

--- main.go
+++ main.go
@@ -10,15 +10,9 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// env
-	errEnv := godotenv.Load()
-	if errEnv != nil {
-		panic("Failed to load env file")
-	}
 
 	// initial DB
 	mysql.DatabaseInit()
